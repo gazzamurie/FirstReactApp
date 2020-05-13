a86c7937bf7489e9b6afe6acbb17c476e0ff296e
GARYS-PC\Gary <40210109@live.napier.ac.uk>
2020-05-13 13:08:58 +0100

    Updates

---
+++ precache-manifest.d8584553bf440d9ba2db38292549cb54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25a6d1c48ec59fdc059059fcddbc4e0",
+    "url": "/FirstReactApp/index.html"
+  },
+  {
+    "revision": "cf14f77e50edd53dea77",
+    "url": "/FirstReactApp/static/css/main.16626470.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/FirstReactApp/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstReactApp/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf14f77e50edd53dea77",
+    "url": "/FirstReactApp/static/js/main.4096f514.chunk.js"
+  },
+  {
+    "revision": "8f3e9ca283a6ae759474",
+    "url": "/FirstReactApp/static/js/runtime-main.a1d97210.js"
+  }
+]);
